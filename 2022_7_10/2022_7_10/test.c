3f59ecefe460198405ae2a0beb047a044234c35e
Jinshuhai <108967664+Jinshuhai@users.noreply.github.com>
2022-07-10 12:04:16 +0800

    Add files via upload

---
+++ 2022_7_10/2022_7_10/test.c
@@ -0,0 +1,166 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+//int main()//单位转换
+//{
+//	int feet, fathoms;
+//
+//	fathoms = 2;
+//
+//	feet = 6 * fathoms;
+//
+//	printf("There are %d feet in %d fathoms!\n", feet, fathoms);
+//	printf("Yes, I said %d feet!\n", 6 * fathoms);
+//	return 0;
+//}
+
+//void butler();
+//int main()//两个函数
+//{
+//	printf("I will summon the butler function.\n");
+//
+//	butler();//调用自定义函数
+//
+//	printf("Yes, bring me some tea and writeable DVDs.\n");
+//	return 0;
+//}
+//
+//void butler()
+//{
+//	printf("You rang, sir?\n");
+//}
+
+//int main(void)
+//{
+//	int a, b;
+//
+//	a = 5;
+//	b = 2;
+//	b = a;
+//	a = b;
+//	printf("%d %d\n", b, a);
+//	return 0;
+//}
+
+//int main(void)
+//{
+//	int x, y;
+//
+//	x = 10;
+//	y = 5;
+//	y = x + y;
+//	x = x * y;
+//
+//	printf("%d %d\n", x, y);
+//	return 0;
+//}
+
+//int main(void)//练习题1
+//{
+//	printf("Gustav Mahler\n");
+//	printf("Gustav\nMahler\n");
+//	printf("Gustav");
+//	printf(" Mahler\n");
+//	return 0;
+//}
+
+//int main(void)//练习题2
+//{
+//	printf("Jinshuhai Beijing\n");
+//	return 0;
+//}
+
+//int main(void)//练习题3
+//{
+//	int age;
+//	int days;
+//
+//	age = 21;
+//	days = age * 365;
+//
+//	printf("age = %d, days = %d", age, days);
+//	return 0;
+//}
+
+//void jolly(void);
+//void deny(void);
+//int main(void)
+//{
+//	jolly();
+//	jolly();
+//	jolly();
+//	deny();
+//	return 0;
+//}
+//
+//void jolly(void)
+//{
+//	printf("for he's a jolly good fellow!\n");
+//
+//}
+//
+//void deny(void)
+//{
+//	printf("Which nobody can deny!\n");
+//}
+
+//void br(void);
+//void ic(void);
+//int main(void)
+//{
+//	br();
+//	ic();
+//	ic();
+//	br();
+//	return 0;
+//}
+//
+//void br()
+//{
+//	printf("Brazil, Russia\n");
+//}
+//void ic()
+//{
+//	printf("India, China\n");
+//}
+
+//int main(void)
+//{
+//	int toes;
+//	int toes2;
+//	int toes_2;
+//	
+//	toes = 10;
+//	toes2 = toes * 2;
+//	toes_2 = toes * toes;
+//
+//	printf("toes = %d\n", toes);
+//	printf("Twice toes = %d\n", toes2);
+//	printf("toes squared = %d\n", toes_2);
+//
+//	return 0;
+//}
+
+//void one_three(void);
+//void two(void);
+//int main(void)
+//{
+//	printf("starting now;\n");
+//	one_three();
+//	printf("done!\n");
+//
+//	return 0;
+//}
+//
+//void one_three()
+//{
+//	printf("one\n");
+//	two();
+//	printf("three\n");
+//}
+//
+//void two()
+//{
+//	printf("two\n");
+//}
